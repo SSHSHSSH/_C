6c7b75f24f86d109184e7c0fbb243976e42bda03
SSHSHSSH <he2367991090@gmail.com>
2025-09-05 23:49:42 +0800

    9/5

--- 二.C语言初阶/一.分支和循环语句/练习/test.c
+++ 二.C语言初阶/一.分支和循环语句/练习/test.c
@@ -79,8 +79,8 @@ int main(){
 } */
 
 
-//
-#include <stdio.h>
+//输入三个数，按从大到小的顺序输出
+/* #include <stdio.h>
 
 int main(){
     int a = 2;
@@ -101,5 +101,169 @@ int main(){
     else if(c < b && b < a)
         printf("%d %d %d\n", a, b, c);//从大到小输出
 
+    return 0;
+} */
+
+
+/* #include <stdio.h>
+
+int main(){
+    printf("%c\n", 'a');
+    printf("%c\n", 97);
+    return 0;
+} */
+
+
+//字符数组练习
+//将字符数组中的内容打印出来，要求：不能改变数组内容，不能使用字符串处理函数
+/* #include <stdio.h>
+
+int main(){
+    int arr[] = {73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33};
+    //sizeof(arr);计算的是总数组的大小（单位是字节）
+    //sizeof(arr[0]);计算的是数组第一个元素的大小（单位是字节）
+    //两者相除就是数组元素的个数
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    int i = 0;
+    while(i < sz){
+        printf("%c", arr[i]);
+        i++;
+    }
+} */
+
+
+//出生日期输入输出
+/* #include <stdio.h>
+
+int main(){
+    int year = 0;
+    int month = 0;
+    int day = 0;
+    scanf("%4d%2d%2d", &year, &month, &day);//%4d表示读入4位整数，%2d表示读入2位整数
+    printf("year = %4d\n", year);
+    printf("month = %02d\n", month);//%02d  2表示打印两位，0表示不足两位时用0补齐
+    printf("day = %02d\n", day);
+    return 0;
+} */
+
+
+
+//学生成绩输入输出
+/* #include <stdio.h>
+
+int main(){
+
+    int id = 0;
+    float c = 0.0f;//初始化赋值0.0时会自动转换为double类型，所以加上f表示float类型
+    float math = 0.0f;
+    float english = 0.0f;
+    //输入
+    scanf("%d;%f,%f,%f", &id, &c, &math, &english);//; , 作为分隔符
+
+    //输出
+    printf("The each subject score of No.%d is %.2f, %.2f, %.2f\n", id, c, math, english);
+    //%.2f表示输出浮点数时保留小数点后两位,而%02f表示输出浮点数时总共占两位，不足两位时用0补齐
+    return 0;
+} */
+
+
+//printf的返回值
+/* #include <stdio.h>
+
+int main(){
+    int n = printf("Hello World!");
+    printf("\n%d\n", n);//n表示打印的字符个数
+    return 0;
+} */
+
+
+
+//转义字符练习
+/* #include <stdio.h>
+
+int main(){
+    printf("printf(\"Hello World!\");\n");
+    printf("cout<<\"Hello World!\"<<endl;");
+    return 0;
+} */
+
+
+
+
+
+//输入四个整数，求最大值
+
+//方法一：放入数组存起来，使用循环找最大值
+/* #include <stdio.h>
+
+int main(){
+    int arr[4] = {0};
+    int i = 0;
+    while(i < 4){
+        scanf("%d", &arr[i]);
+        i++;
+    }
+    int max = arr[0];
+    i = 1;
+    while(i < 4){
+        if(arr[i] > max){
+            max = arr[i]; 
+        }
+        i++;
+    }
+    printf("max = %d\n", max);
+    return 0;
+} */
+
+
+//方法二：不放入数组，直接比较
+/* #include <stdio.h>
+
+int main(){
+    int i = 1;
+    int max = 0;
+    int n = 0;
+    scanf("%d", &max);
+
+    while(i < 4){
+        scanf("%d", &n);
+        if(n > max)
+            max = n;
+        i++;
+    }
+    printf("max = %d\n", max);
+    return 0;
+} */
+
+
+
+//计算求的体积
+/* #include <stdio.h>
+
+int main(){
+    double r = 0.0;//编译器默认0.0为double类型，加上f表示float类型
+    double Π = 3.1415926;
+    double v = 0.0;
+    //输入半径
+    scanf("%lf", &r);
+    //计算体积
+    v = 4/3.0 * Π * r * r * r;
+    //输出体积
+    printf("v = %.3lf\n", v);//double类型输出使用%lf
+    return 0;
+} */
+
+
+
+
+#include <stdio.h>
+
+int main(){
+    int ht = 0;
+    int wt = 0;
+    
+    scanf("%d %d", &wt, &ht);
+    float bmi = wt / ((ht/100.0) * (ht/100.0));
+    printf("bmi = %.2f\n", bmi);
     return 0;
 }
